3a58731e4a115c8d44b6adf3f15fdd9d00d2bba8
Bramantio Ananda <117981449+23Senna@users.noreply.github.com>
2026-09-18 08:15:59 +0700

    Update gpio.c

--- gpio.c
+++ gpio.c
@@ -41,7 +41,11 @@ void MX_GPIO_Init(void)
 
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOC_CLK_ENABLE();
   __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOC, IN3_Pin|IN2_Pin|IN1_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOB, R_EN_Pin|L_EN_Pin, GPIO_PIN_RESET);
@@ -51,6 +55,19 @@ void MX_GPIO_Init(void)
   GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(ENCODER_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : IN3_Pin IN2_Pin IN1_Pin */
+  GPIO_InitStruct.Pin = IN3_Pin|IN2_Pin|IN1_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : BTN_START_STOP_Pin */
+  GPIO_InitStruct.Pin = BTN_START_STOP_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  HAL_GPIO_Init(BTN_START_STOP_GPIO_Port, &GPIO_InitStruct);
 
   /*Configure GPIO pins : R_EN_Pin L_EN_Pin */
   GPIO_InitStruct.Pin = R_EN_Pin|L_EN_Pin;
